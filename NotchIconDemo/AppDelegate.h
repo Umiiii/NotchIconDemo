7ec64dd48770738759c7b5792bab9a3d77de20b9
umi <9@umi.cat>
2019-03-13 16:46:10 +0800

    Initial Commit

---
+++ NotchIconDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotchIconDemo
+//
+//  Created by Cirno on 2019/3/13.
+//  Copyright © 2019 mistylake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotchIconDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotchIconDemo
+//
+//  Created by Cirno on 2019/3/13.
+//  Copyright © 2019 mistylake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
