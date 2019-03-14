7a7ec08cd28797bcd9cabec5fe1af9f190ff986e
umi <9@umi.cat>
2019-03-14 16:26:45 +0800

    First commit

--- NotchIconDemo/ViewController.m
+++ NotchIconDemo/ViewController.m
@@ -14,9 +14,54 @@
 
 @implementation ViewController
 
+- (void)setNotchIcon{
+
+    /* Label Setting */
+    NSString* localizedAppName = [[[NSBundle mainBundle] localizedInfoDictionary]
+                                  objectForKey:@"CFBundleDisplayName"];
+
+    UIImage* appIcon = [UIImage imageNamed:@"AppIcon"];
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+    UILabel* localizedAppNameLabel = [[UILabel alloc]initWithFrame:
+                                      CGRectMake(20 + 15,0,screenWidth - 40 - 15,20)];
+    UIFont* labelFont = [UIFont systemFontOfSize:11.0f];
+    localizedAppNameLabel.font = labelFont;
+    localizedAppNameLabel.textAlignment = NSTextAlignmentCenter;
+    localizedAppNameLabel.text = localizedAppName;
+    CGSize localizedAppNameLabelSize = [localizedAppNameLabel.text
+                                        sizeWithAttributes:
+                                         [NSDictionary dictionaryWithObjectsAndKeys:
+                                          labelFont,NSFontAttributeName,nil]
+                                        ];
+    /* AppIcon Setting */
+    UIImageView* appIconImageView = [[UIImageView alloc]initWithFrame:
+                                     CGRectMake((screenWidth - localizedAppNameLabelSize.width) / 2 - 15, 3, 15, 15)];
+    appIconImageView.layer.masksToBounds = YES;
+    appIconImageView.image = appIcon;
+    appIconImageView.layer.cornerRadius = 2.0f;
+    appIconImageView.layer.borderColor = [UIColor blackColor].CGColor;
+
+
+    /* Add it to status bar */
+    UIWindow* keyWindow = [[UIApplication sharedApplication] keyWindow];
+    [keyWindow addSubview:localizedAppNameLabel];
+    [keyWindow addSubview:appIconImageView];
+
+}
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UITableView* tableView = [[UITableView alloc]initWithFrame:
+                              CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width,
+                                               [UIScreen mainScreen].bounds.size.height)
+                                                         style:UITableViewStyleGrouped];
+    [self.view addSubview:tableView];
+    if(UIApplication.sharedApplication.keyWindow.safeAreaInsets.bottom > 0.0){
+        [self setNotchIcon];
+    }
+
 }
 
 
---
+++ README.md
@@ -0,0 +1,10 @@
+#  NotchIcon Demo
+A simple but elegant solution for adding app icon on the notch when screenshoted.
+Talk is cheap, here is code.
+![ScreenShot-Chinese](https://raw.githubusercontent.com/Umiiii/NotchIconDemo/Screenshot-Chinese.png)
+![ScreenShot-English](https://raw.githubusercontent.com/Umiiii/NotchIconDemo/Screenshot-English.png)
+
+
+## Feature
+- i18n app name supported
+![Notch](https://raw.githubusercontent.com/Umiiii/NotchIconDemo/NotchHeight.png)
